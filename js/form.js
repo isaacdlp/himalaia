9aaaefecfef136581bf30ebd94cd0572e5b61d2f
Isaac de la Peña <admin@agoraeafi.com>
2021-07-29 22:35:34 -0700

    Form secured with CAPTCHA

--- js/form.js
+++ js/form.js
@@ -18,43 +18,50 @@ $(function() {
         // Stop the browser from submitting the form.
         e.preventDefault();
 
-        // Serialize the form data.
-        var formData = $(form).serialize();
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6Ld1dMwbAAAAALM4WzgGBZzCrxpDwPOpVbViieC-', {action: 'submit'}).then(function(token) {
 
-        var msgSending = 'Enviando el mensaje. Un momento';
-        var msgSuccess = 'Mensaje enviado. Responderemos en breve';
-        var msgFailure = 'Mensaje no enviado. Por favor reinténtelo';
-        var msgError = 'Oops! Hubo un error y no se pudo enviar su mensaje'
+                var msgSending = form.attr("msg-sending");
+                var msgSuccess = form.attr("msg-success");
+                var msgFailure = form.attr("msg-failure");
+                var msgError = form.attr("msg-error");
 
-        msgSending = form.attr("msg-sending");
-        msgSuccess = form.attr("msg-success");
-        msgFailure = form.attr("msg-failure");
-        msgError = form.attr("msg-error");
+                if (! msgSending) msgSending = 'Sending message. Just a second';
+                if (! msgSuccess) msgSuccess = 'Message sent. We will be in touch shortly';
+                if (! msgFailure) msgFailure = 'Message not sent. Please try again';
+                if (! msgError) msgError = 'Oops! An error occured and your message could not be sent.';
 
-        form_msg('bg-warn', msgSending);
-        // Submit the form using AJAX.
-        $.ajax({
-            type: 'POST',
-            url: $(form).attr('action'),
-            data: formData
-        })
-        .done(function(response) {
-            if (response.status == "success") {
-                // Make sure that the formMessages div has the 'success' class.
-                form_msg('bg-success', msgSuccess);
-            } else {
-                // Make sure that the formMessages div has the 'success' class.
-                form_msg('bg-danger', msgFailure);
-            }
+                $('#token').val(token);
 
-            // Clear the form.
-            $('#name, #email, #message').val('');
-        })
-        .fail(function(data) {
-            // Make sure that the formMessages div has the 'error' class.
-            form_msg('bg-danger', msgError);
+                // Serialize the form data.
+                var formData = $(form).serialize();
+
+                form_msg('bg-warn', msgSending);
+                // Submit the form using AJAX.
+                $.ajax({
+                    type: 'POST',
+                    url: $(form).attr('action'),
+                    data: formData
+                })
+                    .done(function(response) {
+                        if (response.status == "success") {
+                            // Make sure that the formMessages div has the 'success' class.
+                            form_msg('bg-success', msgSuccess);
+                        } else {
+                            // Make sure that the formMessages div has the 'danger' class.
+                            form_msg('bg-danger', msgFailure);
+                        }
+
+                        // Clear the form.
+                        $('#name, #email, #message').val('');
+                    })
+                    .fail(function(data) {
+                        // Make sure that the formMessages div has the 'danger' class.
+                        form_msg('bg-danger', msgError);
+                    });
+
+            });
         });
-
     });
 
 });
